5737e292659a9c0b7c18a87c5e490b3959f4f06c
njeeegos <njegos@elfak.rs>
2021-03-18 00:41:21 +0100

    ParkirajVozilo

--- Controllers/ParkingController.cs
+++ Controllers/ParkingController.cs
@@ -18,6 +18,79 @@ namespace WebProg_Projekat_2021.Controllers
         {
             Context=context;
         }
+        /*
+        [HttpPost]
+        [Route("KreirajParking")]
+        public async Task KreirajParking([FromBody] Parking p)
+        {
+            var proba=Context.Parkinzi.Add(p);
+            await Context.SaveChangesAsync();
+
+            //await KreirajMesta(parking.Ime);
+
+            var parking=Context.Parkinzi.Where(prom => prom.Ime==p.Ime).FirstOrDefault();
+
+            for(var i=0;i<(parking.X*parking.Y);i++)
+            {
+                var mesto=new Mesto();
+                mesto.Broj=i+1;
+                mesto.Vozilo=null;
+                mesto.Parking=parking;
+                Context.Mesta.Add(mesto);
+            }
+            await Context.SaveChangesAsync();
+
+
+        }
+        */
+
+        [HttpPost]
+        [Route("KreirajParking")]
+        public async Task KreirajParking([FromBody] Parking parking)
+        {
+            Context.Parkinzi.Add(parking);
+            await Context.SaveChangesAsync();
+
+            for(var i=0;i<(parking.X*parking.Y);i++)
+            {
+                var mesto=new Mesto();
+                mesto.Broj=i+1;
+                mesto.Vozilo=null;
+                mesto.Parking=parking;
+                Context.Mesta.Add(mesto);
+                await Context.SaveChangesAsync();
+
+                parking.Mesta.Add(mesto);
+            }
+            
+            Context.Update<Parking>(parking);
+            await Context.SaveChangesAsync();
+        }
+
+        [HttpDelete]
+        [Route("ObrisiParking/{idParkinga}")]
+        public async Task ObrisiParking(int idParkinga)
+        {
+            var parking= await Context.Parkinzi.FindAsync(idParkinga);
+            Context.RemoveRange(Context.Mesta.Where(m => m.Parking.ParkingId==idParkinga));
+            Context.Remove(parking);
+            await Context.SaveChangesAsync();
+        }
+
+        [HttpPut]
+        [Route("ParkirajVozilo/{idParkinga}/{brojMesta}")]
+        public async Task ParkirajVozilo(int idParkinga, int brojMesta, [FromBody] Vozilo vozilo)
+        {
+            Context.Vozila.Add(vozilo);
+            await Context.SaveChangesAsync();
+
+            var parking=await Context.Parkinzi.FindAsync(idParkinga);
+            var mesto=parking.Mesta.Where(p => p.Broj==brojMesta).FirstOrDefault();
+            mesto.Vozilo=vozilo;
+
+            Context.Update<Mesto>(mesto);
+            await Context.SaveChangesAsync();
+        }
 
     }
 }
